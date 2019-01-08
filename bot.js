b95c3add9756dbdd14d9629fcf3690921f21959d
ahmedmaher10 <43995604+ahmedmaher10@users.noreply.github.com>
2019-01-08 18:41:59 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,7 +20,7 @@ const gif = require("gif-search");
 
 const client = new Discord.Client({disableEveryone: true});
 
-const prefix = "5";
+const prefix = "1";
 /////////////////////////
 ////////////////////////
 
