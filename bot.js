cd854819d6511fc821b30bac6605a405b3e53c4e
ahmedmaher10 <43995604+ahmedmaher10@users.noreply.github.com>
2019-02-02 22:17:28 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -395,19 +395,5 @@ client.on('guildMemberAdd', member=> {
     var role = member.guild.roles.find("name","!Givenchy Mamber");
     member.addRole(role);
 });
-var prefix = "$";
 
-client.on("message", message => {
-
-            if (message.content.startsWith(prefix + "obc")) {
-                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
-  let args = message.content.split(" ").slice(1);
-  var argresult = args.join(' '); 
-  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
- m.send(`${argresult}\n ${m}`);
-})
- message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`); 
- message.delete(); 
-};     
-});
 client.login(process.env.BOT_TOKEN);
